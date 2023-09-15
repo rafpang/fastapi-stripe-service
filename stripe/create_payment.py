cb2670770e4ac3db267aef2c654ff80a6005de73
rafpang <rafibintangpagi@gmail.com>
2023-09-15 16:38:08 +0800

    removed unnecessary stripe dependency injection

--- stripe/create_payment.py
+++ stripe/create_payment.py
@@ -1,16 +1,17 @@
 from fastapi import APIRouter, HTTPException, Depends
 from celery.create_payment_task import create_payment_task
 from request_model.request_models import PaymentRequest
-from db.db_models import Payment
+
 from celery.process_successful_payment_task import process_successful_payment
 
-from .stripe_init import get_stripe_api_key
 
 router = APIRouter(prefix="/payment")
 
 
 @router.post("/create-payment")
-def create_payment(payment_request: PaymentRequest, stripe=Depends(get_stripe_api_key)):
+def create_payment(
+    payment_request: PaymentRequest,
+):
     try:
         payment_task_response = create_payment_task.apply_async(args=[payment_request])
         payment_id = payment_task_response["paymentId"]
