9b6a16aff2023dd966316844737d15abcb3f860b
rafpang <rafibintangpagi@gmail.com>
2023-09-16 11:47:26 +0800

    added .env for secret stripe key

--- app/stripe/stripe_init.py
+++ app/stripe/stripe_init.py
@@ -1,6 +1,10 @@
 import stripe
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 
 def get_stripe_instance():
-    stripe.api_key = "your_stripe_secret_key"
+    stripe.api_key = os.environ.get("STRIPE_SECRET_KEY")
     return stripe
